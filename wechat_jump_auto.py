554dca0acf246acdd4b55c4a2db34f1e6434c681
yuanjianye <yuanjianye@126.com>
2018-01-03 11:53:54 +0800

    commit pyfiles
    Signed-off-by: yuanjianye <yuanjianye@126.com>

--- wechat_jump_auto.py
+++ wechat_jump_auto.py
@@ -89,64 +89,17 @@ def get_distance():
                 candidate_center_y = y + h/2
             else:
                 candidate_center_y = (candidate_center_y + y + h)/2
-    distance = (candidate_center_y - cir_center_y) * (candidate_center_y - cir_center_y) + (candidate_center_x - cir_center_x) * (candidate_center_x - cir_center_x)
+    distance = math.pow((candidate_center_y - cir_center_y),2) + math.pow((candidate_center_x - cir_center_x),2)
     img4 = copy.deepcopy(img2)
     img4 = cv2.drawContours(img4, candidate_cnts, -1, (0, 0, 0), 8)
-    #print(len(candidate_cnts))
-    #print(distance)
-    #print(candidate_cnts)
+
+    print("candidate_cnts count = %d" % len(candidate_cnts))
+    print("distance = %d" % distance)
+
     show_imgs([img1,img2,img3,img4,img5,mask])
     return distance
-
 
 if __name__ == "__main__":
     distance=get_distance()
     touch_emulate(distance*7)
 
-
-'''
-def filtercont2(cnt):
-    x,y,w,h=cv2.boundingRect(cnt)
-    if w > 30 and w < 60 and h > 30 and h < 60 and w/h > 0.95 and w/h < 1.05:
-        return True
-    else:
-        return False
-'''
-
-#newimg2 = cv2.equalizeHist(newimg)
-#newimg = cv2.GaussianBlur(newimg)
-'''
-ret,bimg1=cv2.threshold(newimg,210,255,cv2.THRESH_BINARY)
-bimg2=cv2.adaptiveThreshold(newimg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,3)
-
-
-cimg,cnts,hierarchy = cv2.findContours(bimg2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(cimg,cnts,-1,(0,0,255),1)
-cimg,cnts,hierarchy = cv2.findContours(bimg2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cnts.sort(key=contsize)
-cnts2 = [ x for x in cnts if filtercont(x) or filtercont2(x)]
-print(np.size(cnts2))
-#cv2.drawContours(cimg,cnts[-2:-1],-1,(0,0,255),5)
-cv2.drawContours(cimg,cnts2,-1,(0,0,255),5)
-#cv2.drawContours(cimg,cnts[0:60],-1,(0,0,255),5)
-
-
-
-
-images = [newimg,bimg2,cimg]
-
-
-plt.figure()
-for i in range(len(images)):
-    plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
-#plt.subplot(2,3,len(images)+1),plt.plot(hist_cv)
-plt.show()
-'''
-#win=cv2.namedWindow("test win",flags=0)
-#cv2.imshow('test win',bimg1,bimg2,bimg3)
-#cv2.waitKey(0)
-#plt.imshow(newimg)
-#plt.show()
-#myimg=cv2.threshold(myimg,30,200,cv2.THRESH_BINARY)
-#plt.imshow(imutils.opencv2matplotlib(newimg))
-#plt.show()
